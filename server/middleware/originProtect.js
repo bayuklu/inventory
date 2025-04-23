d6d7ff6095d73a8f723825a91ebd8391e7d0a6f0
MRXbef <adambayu878@gmail.com>
2025-04-23 21:10:02 +0800

    update fitur login

--- server/middleware/originProtect.js
+++ server/middleware/originProtect.js
@@ -1,5 +1,6 @@
 const protectOrigin = (req, res, next) => {
     const allowerOrigin = ['https://abfrozen.vercel.app']
+    console.log(req)
     const ip = req.headers['x-vercel-forwarded-for']
     if(allowerOrigin.includes(ip)) {
         next()
