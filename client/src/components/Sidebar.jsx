d15ccc23d5b751b5b9da8b3514a8c6a92814d109
MRXbef <adambayu878@gmail.com>
2025-04-25 19:51:43 +0800

    update fitur login

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-import '../index.css'
+import "../index.css";
+import axios from "axios";
+import {useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = async() => {
+    try {
+      const response = await axios.delete(`${import.meta.env.VITE_BASEURL}/logout`)
+      if(response) navigate("/login")
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
   return (
     <div className="my-sidebar">
       <aside className="menu">
@@ -21,6 +33,11 @@ const Sidebar = () => {
         <ul className="menu-list">
           <li>
             <a href="cashier">Cashier</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#" onClick={handleLogout}>Log Out</a>
           </li>
         </ul>
       </aside>
