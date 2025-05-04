b29627360539dac7849ba1ad7a648ee4421ff15d
MRXbef <adambayu878@gmail.com>
2025-05-04 19:27:49 +0800

    update inventory

--- server/index.js
+++ server/index.js
@@ -41,8 +41,8 @@ try {
 }
 
 app.use(cors({credentials: true, 
-    // origin: "http://localhost:5173",
-    origin: "https://abfrozen.vercel.app"
+    origin: "http://localhost:5173",
+    // origin: "https://abfrozen.vercel.app"
 }))
 
 app.use(cookieParser())
