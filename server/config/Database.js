39369c3237b01b3a306ed02f629a3089e9001513
MRXbef <adambayu878@gmail.com>
2025-05-10 18:05:48 +0800

    membuat fungsi migrate database dan memperbaiki config database sesuai lingkungan

--- server/config/Database.js
+++ server/config/Database.js
@@ -4,31 +4,53 @@ import pg from 'pg'
 
 dotenv.config()
 
-const db = new Sequelize(
-    // process.env.PRODUCTION_PSQL,
-    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
-    {
-    dialect: "postgres",
-    protocol: "postgres",
-    timezone: "UTC",
-    dialectOptions: {
-        // ======= gunakan ini jika di dalam lingkungan production ========
-        // ssl: {
-        //     require: true,
-        //     rejectUnauthorized: false
-        // },
-        // ======= gunakan ini jika di dalam lingkungan production ========
-        useUTC: true,
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000,
-        acquire: 30000,
-    },
-    logging: false,
-    dialectModule: pg
-})
+let db
+
+if(process.env.NODE_ENV === "PRODUCTION")
+{
+    db = new Sequelize(
+        process.env.PRODUCTION_PSQL,
+        {
+        dialect: "postgres",
+        protocol: "postgres",
+        timezone: "UTC",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            },
+            useUTC: true,
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000,
+            acquire: 30000,
+        },
+        logging: false,
+        dialectModule: pg
+    })  
+}else {
+    db = new Sequelize(
+        process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
+        {
+        dialect: "postgres",
+        protocol: "postgres",
+        timezone: "UTC",
+        dialectOptions: {
+            useUTC: true
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000,
+            acquire: 30000,
+        },
+        logging: false,
+        dialectModule: pg
+    })
+}
+
 
 
 
