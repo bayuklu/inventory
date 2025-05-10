5ec30d125b1e391686720934dbd77bc4e8d8e2d2
MRXbef <adambayu878@gmail.com>
2025-05-10 18:11:22 +0800

    membuat fungsi migrate database dan memperbaiki config database sesuai lingkungan

--- server/config/MysqlDb.js
+++ server/config/MysqlDb.js
@@ -1,11 +1,12 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv'
+import mysql from 'mysql2'
 
 dotenv.config()
 
 const mysqlDb = new Sequelize ('db_old_ab', 'root', '', {
     host: "localhost",
-    dialect: "mysql",
+    dialect: mysql,
     timezone:"+08:00"
 })
 
