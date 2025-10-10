b30d38fdea40ba5114c91c6c8f2b454738164856
MRXbef <adambayu878@gmail.com>
2025-10-10 20:16:28 +0800

    payment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,9 @@ axios.defaults.withCredentials = true
 import './index.css'
 import 'bulma/css/bulma.css'
 
+// replace your newest transaction payment here!
+const payment_transaction_id = "290188881329003"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
