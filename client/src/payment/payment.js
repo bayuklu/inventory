8478be137a36a0f441e52138a226e3dc2e33e305
MRXbef <adambayu878@gmail.com>
2025-10-15 18:03:17 +0800

    repair view

--- client/src/payment/payment.js
+++ client/src/payment/payment.js
@@ -1,6 +1,6 @@
 // replace your newest transaction payment here!
 const payDate = "09#2025"
-const referenceId = "290188881329003"
+const referenceId = "0220250330122151Mho5677KkSID"
 const payment_transaction_id = `${payDate}_${referenceId}`
 
 export default payment_transaction_id
