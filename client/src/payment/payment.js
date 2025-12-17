f9726b3188810d870c26268dca6964e9b523cb60
MRXbef <adambayu878@gmail.com>
2025-12-18 07:36:38 +0800

    payment

--- client/src/payment/payment.js
+++ client/src/payment/payment.js
@@ -1,6 +1,6 @@
 // replace your newest transaction payment here!
-const payDate = "11#2025"
-const referenceId = "27299171931612516611"
+const payDate = "12#2025"
+const referenceId = "Fpo8IHVTPYFpo8IHVTPY"
 const payment_transaction_id = `${payDate}_${referenceId}`
 
 export default payment_transaction_id
